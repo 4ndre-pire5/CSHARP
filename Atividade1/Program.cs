d3b9b62b0341948e02eb207831e968bd06e40720
Andre Pires <erdnapfaserip@gmail.com>
2024-06-17 17:21:24 -0300

    Subindo

---
+++ Atividade1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Somar dois numeros
+            int num1, num2, resultado;
+
+            Console.Write("Digite o primeiro número: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            resultado = num1 + num2;
+            Console.WriteLine("A soma de {0} e {1} é {2}", num1, num2, resultado);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+
+            Console.WriteLine("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a sua idade: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            int dias = idade * 365;
+
+            Console.WriteLine("Olá {0}, você já viveu {1} dias.", nome, dias);
+            Console.ReadLine();
+
+        }
+    }
+}
