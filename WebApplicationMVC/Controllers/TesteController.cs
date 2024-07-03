982ff9c4cb234dd2b7e6520c6bd9461ba99b6b79
Andre Pires <erdnapfaserip@gmail.com>
2024-07-03 15:02:47 -0300

    Web App MVC

---
+++ WebApplicationMVC/Controllers/TesteController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationMVC.Models;
+
+namespace WebApplicationMVC.Controllers
+{
+    public class TesteController : Controller
+    {
+        public string Exemplo01()
+        {
+            return "Isto é um método.";
+        }
+
+        public ViewResult Exemplo02()
+        {
+            return View();
+        }
+
+        public ViewResult Exemplo03()
+        {
+            var fornecedor = new FornecedorViewModel();
+            fornecedor.Nome = "Papelaria ASP.NET";
+            fornecedor.Email = "papel@asp.net";
+            fornecedor.Telefone = "1234-5678";
+            return View(fornecedor);
+        }
+    }
+}
