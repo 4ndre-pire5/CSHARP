cc1e30d2dbfdc2c4312541d7affd5975a068831c
Andre Pires <erdnapfaserip@gmail.com>
2024-07-04 09:15:25 -0300

    Exemplo Formulario

--- ExemploMVC/Controllers/HomeController.cs
+++ ExemploMVC/Controllers/HomeController.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplicationMVC.Models;
 
 namespace ExemploMVC.Controllers
 {
     public class HomeController : Controller
     {
+
+        public ViewResult FornecedorIncluir()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult FornecedorIncluir(FornecedorViewModel fornecedor)
+        {
+            if (fornecedor.Nome == null)
+            { 
+                //Tratamento...
+            }
+            return View();
+        }
+
 
         public ViewResult Servicos()
         { 
